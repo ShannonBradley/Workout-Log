f931f8b02e5037eb80045e35d36da24ccd06595c
Shannon Bradley <mrktguru@gmail.com>
2021-01-28 15:33:00 -0600

    updates to project

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -20,17 +20,9 @@ router.post('/create', validateSession, (req, res) => {
     .catch(err => res.status(500).json ({error: err}))
     });
 
-    //Get all logs
-
-    router.get("/", (req, res) => {
-    Log.findAll()
-        .then(logs => res.status(200).json(logs))
-        .catch(err => res.status(500).json({ error: err }));
-    });
-
     //Get all logs for single user
 
-    router.get("/mine", validateSession, (req, res) => {
+    router.get("/", validateSession, (req, res) => {
         let userid = req.user.id
         Log.findAll({
           where: {owner_id: userid}
@@ -43,7 +35,7 @@ router.post('/create', validateSession, (req, res) => {
 
     router.get("/:id", function(req, res) {
       let id = req.params.id;
-      Log.findAll({where: {id: id}
+      Log.findAll({where: {owner_id: userid}
       })
       .then(logs => res.status(200).json(logs))
       .catch(err => res.status(500).json({ error: err}))
