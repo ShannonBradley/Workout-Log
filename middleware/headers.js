dc6548bcf71a510f8ff644d5186571fb54d137fc
Shannon Bradley <mrktguru@gmail.com>
2021-02-09 14:48:51 -0600

    workout log server

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,10 +1,17 @@
-module.exports = (req, res, next) => {
-    res.header('access-control-allow-origin', '*');
-    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
-    res.header(
-      'access-control-allow-headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+// module.exports = (req, res, next) => {
+//     res.header('access-control-allow-origin', '*');
+//     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+//     res.header(
+//       'access-control-allow-headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
   
-    next();
-  };
+//     next();
+//   };
+
+module.exports = function (req, res, next) {
+  res.header('access-control-allow-origin', '*');
+  res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+  res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+};
